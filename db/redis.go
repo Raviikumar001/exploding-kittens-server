809a721f3da3afa88ab1e8fbc2adfebaa406fd3d
ravikumar-hash <ravi.k@fyle.in>
2024-07-22 22:30:45 +0530

    added new go redis config

--- db/redis.go
+++ db/redis.go
@@ -1,39 +1,72 @@
 package db
 
 import (
-    "context"
-    "fmt"
-    "os"
+	"context"
+	"fmt"
+	"os"
 
-    "github.com/redis/go-redis/v9"
+	"github.com/redis/go-redis/v9"
 )
-
 
 var redisClient *redis.Client
 
-func StartRedis()  {
-    redisURL := os.Getenv("REDIS_DB_URL")
-    
-   
-    redisClient = redis.NewClient(&redis.Options{
-		Addr:     redisURL,
-		Password: os.Getenv("REDIS_DB_PASSWORD"), 
-		DB:       0,                                 
-	})
+func StartRedis() {
+	redisURL := os.Getenv("REDIS_DB_URL")
 
-
-	ping, err := redisClient.Ping(context.Background()).Result()
+	opt, err := redis.ParseURL(redisURL)
 	if err != nil {
-		fmt.Println((err.Error()))
+		fmt.Println("Error parsing Redis URL:", err)
 		return
 	}
-	fmt.Println(ping)
 
-    fmt.Println("Connected to Redis!")
+	redisClient = redis.NewClient(opt)
+
+	ctx := context.Background()
+	ping, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		fmt.Println("Error connecting to Redis:", err)
+		return
+	}
+	fmt.Println("Redis Ping:", ping)
+
+	fmt.Println("Connected to Redis!")
 }
 
 func GetRedisClient() *redis.Client {
-    return redisClient
+	return redisClient
 }
 
+// package db
 
+// import (
+//     "context"
+//     "fmt"
+//     "os"
+
+//     "github.com/redis/go-redis/v9"
+// )
+
+// var redisClient *redis.Client
+
+// func StartRedis()  {
+//     redisURL := os.Getenv("REDIS_DB_URL")
+
+//     redisClient = redis.NewClient(&redis.Options{
+// 		Addr:     redisURL,
+// 		Password: os.Getenv("REDIS_DB_PASSWORD"),
+// 		DB:       0,
+// 	})
+
+// 	ping, err := redisClient.Ping(context.Background()).Result()
+// 	if err != nil {
+// 		fmt.Println((err.Error()))
+// 		return
+// 	}
+// 	fmt.Println(ping)
+
+//     fmt.Println("Connected to Redis!")
+// }
+
+// func GetRedisClient() *redis.Client {
+//     return redisClient
+// }
